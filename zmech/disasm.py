7a76286183592fdc357004747a8ced767b31b216
Sunjay Cauligi <scauligi@gmail.com>
2025-04-23 19:37:20 -0400

    BB_COULDFAIL

--- zmech/disasm.py
+++ zmech/disasm.py
@@ -2,7 +2,8 @@ from attrs import define, field
 
 from .structs import Insn
 
-BB_END = "ret jump rtrue rfalse print_ret restore restart ret_popped quit".split()
+BB_END = "ret jump rtrue rfalse print_ret ret_popped".split()
+BB_COULDFAIL = "restore restart quit".split()
 
 
 @define
@@ -36,12 +37,16 @@ def parse_routine(z, addr=None, header=True, r=None):
         else:
             r.bbs.add(z.tell())
         seen = {insn.addr for insn in r.insns}
+        could_have_failed = False
         while True:
             insn = z.readInsn()
             if not insn:
                 break
             r.insns.append(insn)
             seen.add(insn.addr)
+            if could_have_failed:
+                r.bbs.add(insn.addr)
+                could_have_failed = False
             if insn.name == "call" and insn.dst:
                 r.calls.append(insn)
             elif insn.dst is not None:
@@ -51,4 +56,6 @@ def parse_routine(z, addr=None, header=True, r=None):
             if insn.name in BB_END:
                 if seen.issuperset(r.bbs):
                     break
+            if insn.name in BB_COULDFAIL:
+                could_have_failed = True
     return r
